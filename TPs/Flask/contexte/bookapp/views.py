3e7a19bdfe1b88bcb21e1d834de693c58a6098f7
Moussa <moussainssa@outlook.fr>
2020-11-19 16:38:20 +0100

    TPs
    Excuse du retard je prennais note

--- TPs/Flask/contexte/bookapp/views.py
+++ TPs/Flask/contexte/bookapp/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, jsonify, render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,8 +10,8 @@ def book():
         book=[{'id':1,'titre' : 'un titre',},{'id':2,'titre': 'un autre titre random',}]
         return jsonify(book)
 
-@app.route('/api/books/<id>',methods=['GET'])
-def lebook(id=None):
+@app.route('/api/books/id/<id>',methods=['GET'])
+def lebookbyid(id=None):
         leid = int(id)
         book=[{'id':1,'titre' : 'un titre',},{'id':2,'titre': 'un autre titre random',}]
 
@@ -22,9 +22,22 @@ def lebook(id=None):
         else :
                 data = book
 
-        return render_template('index.html', title="faite une route qui retourne un book selon son id", data=data)
+        return render_template('index.html', title="Les données de book selon son id", data=data)
         #request.json[book[0]['id']][0]
         #return jsonify(book[leid])
 
+@app.route('/api/books/titre/<titre>',methods=['GET'])
+def lebookbytitre(titre=None):
+        letitre = str(titre)
+        book=[{'titre':1,'titre' : 'un titre',},{'titre':2,'titre': 'un autre titre random',}]
+
+        if(letitre == 'un titre'):
+                datatitle = book[0]
+        elif(letitre == 'un autre titre random'):
+                datatitle = book[1]
+        else :
+                datatitle = book
+
+        return render_template('index.html', title="Les données de book selon son titre", datatitre=datatitle)
 if __name__ == "__main__":
         app.run()
